b9bb1f9f980506fd6446639946dd6f9c2799bd89
RussMichal <russell.michal@gmail.com>
2023-10-06 20:15:26 -0500

    adding loops/array lesson

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,188 +1,78 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-//Make a program that converts feet to inches
-void homework()
-{
-	std::cout << "what to convert 1 for ft to in, 0 for oz to g:\n";
-	bool convertType;
-	std::cin >> convertType;
-
-	if (convertType)
-	{
-		std::cout << "Convert feet to inches:\n";
-		float Feet;
-		std::cin >> Feet;
-
-		float Inches = (Feet * 12.0f);
-		std::cout << Feet << " feet is " << Inches << " inches\n";
-	}
-	else
-	{
-		std::cout << "enter a measurement in ounces:";
-		float oz;
-		std::cin >> oz;
-
-		float g = (oz * 28.3495);
-		std::cout << "the measurement in grams is: " << g;
-	}
-}
+#include <time.h>
 
 int main()
 {
-	//russ
-	//binary
-	//more data type
-	//8 bits = 1 byte
-	char; //1 byte 0-255
-	int;  //4 byte
-	float; //4 byte
+    int x;
+    //scope 
+    if (true)
+    {
+        x = 3;
+        x = 4;
+        x = x + 5;
+        //...
+    }
+    else
+    {
+        x = 6;
+        int y = 4;
+        y = 5;
+        y = y + 2;
+        //...
+    }
+    x = 3;
+    //loops
+        //while
+        //do while
+        //for
+    float tempF = -100;
+    do
+    {
+        std::cout << "temp F please: (-100 to stop)";
+        std::cin >> tempF;
 
-	bool b;//1byte; true not 0 or false=0
-	
-	short sh;//2 byte
-	int i;//4 byte
-	long l;//8 byte
-	long long ll;//16 byte
-	unsigned int ui;//4 byte
-	
-	float pi = 3.14;  //4 byte
-	double dpi = 3.14159; //8 byte
+        float tempC = tempF - 32 * (5 / 9.0f);
+        std::cout << "your temp in C is " << tempC << std::endl;
 
-	char; //1 byte 0-255
+    } while (tempF != -100);
+    
+    //random numbers
+    srand(time(NULL));
+    std::cout << "arandom number " << ((float)rand() / RAND_MAX)*10 <<"\n";
 
-	char r = 'r';
-	char u = 'u';
-	char s = 's';
+    //array
+    int employeeAge1 = 19;
+    int employeeAge2 = 29;
+    int employeeAge3 = 39;
 
-	std::string rus = "rus";
-	std::string name = "joan";
-	std::string message = "hello, how are you?";
+    (employeeAge1 + employeeAge2 + employeeAge3) / 3;
 
-	//if
-//T&&T = T
-//T&&F = F
-//F&&T = F
-//F&&F = F
+    int employeeAge[10] = { 0,1,2,3,4,5,6,7,8,9 };//0...9
 
-//T||T = T
-//T||F = T
-//F||T = T
-//F||F = F
+    //employeeAge[0] + employeeAge[1] + employeeAge[2] + employeeAge[3] + employeeAge[4] +
 
-	int instrumentCount;
-	std::cout << "how many instruments do you play?";
-	std::cin >> instrumentCount;
-	
-	bool clarinet;
-	std::cout << "do you play clarinet?";
-	std::cin >> clarinet;
+    int total=0;
+    for (int i = 0; i < 10; i++)//0...9
+    {
+        total+=employeeAge[i];
+    }
 
-	bool piano;
-	std::cout << "do you play paino?";
-	std::cin >> piano;
+    std::cout<<"avg "<<total / 10<<"\n";
 
-	if (instrumentCount > 2 && clarinet)
-	{
-		std::cout << "you play clarinet and ...";
-	}
-	else if (instrumentCount <= 0)
-	{
-		std::cout << "you dont play";
-	}
-	else if ((piano || clarinet) && instrumentCount==1)
-	{
-		std::cout << "you only play clarinet or paino";
-	}
+    //for each
+    total = 0;
+    for (int element: employeeAge)//0...9
+    {
+        total += element;
+    }
 
-	//switch
-	int age = 1;
-	//switch
-	switch (age)
-	{
-	case 1:
-		std::cout << "age 1\n";
-	case 2:
-		std::cout << "age 2\n";
-		break;
-	case 3:
-		std::cout << "age 3\n";
-		break;
-	case 4:
-		std::cout << "age 4\n";
-		break;
-	default:
-		std::cout << "age default\n";
-		break;
-	}
+    float myFloat;
+    float myFloat2[10];
+    std::string names[10];
+    std::cout << "avg2 " << total / 10 << "\n";
 
+    //create an array of size 100, filled with the fibinochi sequence
+    //0,1,1,2,3,5,8,13...
+    return 0;
 
-	//tax app example
-	int income;
-	std::cout << "whats your income?";
-	std::cin >> income;
-
-	float taxRate;
-	switch (income)
-	{
-	case 10000:
-		taxRate = 0.0;
-		break;
-	case 20000:
-		taxRate = 0.1;
-		break;
-	case 30000:
-		taxRate = 0.2;
-		break;
-	case 40000:
-		taxRate = 1.0;
-		break;
-	default:
-		taxRate = -0.5;
-		break;
-	}
-
-	std::cout << "your tax rate from switch is: "<< taxRate<<"\n";
-
-	if (income < 10000)
-	{
-		taxRate = 0.0;
-	}
-	else if (income > 10000 && income < 20000)
-	{
-		taxRate = 0.1;
-	}
-	else if (income > 20000 && income < 30000)
-	{
-		taxRate = 0.2;
-	}
-	else if (income >= 40000)
-	{
-		taxRate = 1.0;
-	}
-	std::cout << "your tax rate from if is: " << taxRate<<"\n";
-
-	//home work calculator
-	//take a number
-	//take a operation (-,+,*,/)
-	//take another number
-	//do the operation on the numbers
-
-	//one number must be less than 10
-	//other number must be greater then 10
-	//if thats not true then exit
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
