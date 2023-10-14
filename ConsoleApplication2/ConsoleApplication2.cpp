dc808ae991be27c12ee4d7603b7f87d90f04e275
RussMichal <russell.michal@gmail.com>
2023-10-13 20:12:50 -0500

    functions bby!

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,78 +1,129 @@
+// Fibonacci 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
-#include <time.h>
 
-int main()
+void tempConvert()
 {
-    int x;
-    //scope 
-    if (true)
-    {
-        x = 3;
-        x = 4;
-        x = x + 5;
-        //...
-    }
-    else
-    {
-        x = 6;
-        int y = 4;
-        y = 5;
-        y = y + 2;
-        //...
-    }
-    x = 3;
-    //loops
-        //while
-        //do while
-        //for
-    float tempF = -100;
-    do
-    {
-        std::cout << "temp F please: (-100 to stop)";
-        std::cin >> tempF;
+	std::cout << "temp F please: ";
+	float tempF;
+	std::cin >> tempF;
 
-        float tempC = tempF - 32 * (5 / 9.0f);
-        std::cout << "your temp in C is " << tempC << std::endl;
+	float tempC = tempF - 32 * (5 / 9.0);
+	std::cout << "your temp in C is " << tempC << std::endl;
+}
 
-    } while (tempF != -100);
+void ImaniConvertFtIn()
+{
+	std::cout << "Hello" << std::endl;
+	std::string name;
+	float ft_height;
+	float in_height;
+
+	std::cout << "Please enter your name: " << std::endl;
+	std::cin >> name;
+	std::cout << "Please enter your inches in inches: " << std::endl;
+	std::cin >> in_height;
+	ft_height = in_height / 12.0f;
+	std::cout << "Your name is " << name << std::endl;
+	std::cout << "Your height is " << in_height << " inches" << std::endl;
+}
+
+void ImaniConvertKgLb()
+{
+	std::cout << "Hello" << std::endl;
+	std::string name;
+	float kg_weight;
+	float lbs_weight;
+
+	std::cout << "Please enter your name: " << std::endl;
+	std::cin >> name;
+	std::cout << "Please enter your weight in lbs: " << std::endl;
+	std::cin >> lbs_weight;
+	kg_weight = lbs_weight / 2.2046f;
+	std::cout << "Your name is " << name << std::endl;
+	std::cout << "You weight " << kg_weight << " kg" << std::endl;
+}
+
+int max(const int a, const int b)
+{
+	if (a > b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+int max(const int a, const int b, const int c)
+{
+	if (a > b)
+	{
+		return max(a, c);
+	}
+	else
+	{
+		return max(b, c);
+	}
+}
+
+int min(const int a, const int b)
+{
+	if (a < b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+int main() 
+{
+	int aa = 2;
+	int bb = 3;
+	int cc = 3;
+
+	int maxvalue = max(aa, bb,cc);
+
+	int minvalue = min(aa, bb);
+
+	maxvalue = max(aa+5, bb);
+
+	minvalue = min(aa+5, bb);
     
-    //random numbers
-    srand(time(NULL));
-    std::cout << "arandom number " << ((float)rand() / RAND_MAX)*10 <<"\n";
+	int opt=9;
+	while (opt!=0)
+	{
+		std:: cout << "what do you want to convert? 1.ftin 2.kglb 3.temp\n";
+		std::cin >> opt;
+		switch (opt)
+		{
+		case 1:
+			ImaniConvertFtIn();
+			break;
+		case 2:
+			ImaniConvertKgLb();
+			break;
+		case 3:
+			tempConvert();
+			break;
+		}
+	}
+	//functions
+		// arguments
+		// return types
+		// const 
+		//overloading
 
-    //array
-    int employeeAge1 = 19;
-    int employeeAge2 = 29;
-    int employeeAge3 = 39;
+	//array of like 5 numbers init the array with loop to 5,4,3,2,1
+	//print the array <- unsorted print
+	//sort the array least to greatest <- hard
+	//print the array <- sorted print
+	//2-3 functions
 
-    (employeeAge1 + employeeAge2 + employeeAge3) / 3;
-
-    int employeeAge[10] = { 0,1,2,3,4,5,6,7,8,9 };//0...9
-
-    //employeeAge[0] + employeeAge[1] + employeeAge[2] + employeeAge[3] + employeeAge[4] +
-
-    int total=0;
-    for (int i = 0; i < 10; i++)//0...9
-    {
-        total+=employeeAge[i];
-    }
-
-    std::cout<<"avg "<<total / 10<<"\n";
-
-    //for each
-    total = 0;
-    for (int element: employeeAge)//0...9
-    {
-        total += element;
-    }
-
-    float myFloat;
-    float myFloat2[10];
-    std::string names[10];
-    std::cout << "avg2 " << total / 10 << "\n";
-
-    //create an array of size 100, filled with the fibinochi sequence
-    //0,1,1,2,3,5,8,13...
     return 0;
-
 }
