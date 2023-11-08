bd1eff5aa1cb42e356392d0596a53a0dafc5ae80
bgardner1087 <143228703+bgardner1087@users.noreply.github.com>
2023-11-08 14:07:45 -0600

    Add files via upload

--- Multi Function Homework/Multi Function Homework/Array Functions.cpp
+++ Multi Function Homework/Multi Function Homework/Array Functions.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <algorithm> //for ranges
+#include "functions.h"
 
-int main()
-{
-	const int SIZE = 5;
-	int arr[SIZE] = { 5, 4, 3, 2, 1 };
+int arrayFunctions() {
 
+const int SIZE = 5;
+int arr[SIZE] = { 5, 4, 3, 2, 1 };
+
+void initializeArray(); {
+	//Initializes the array
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = 5 - i;
+	}
+}
+
+void printArray(); {
+	// print sorted array
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+}
+
+void sortingArray(); {
+	//sorts the array
 	for (int i = 0; i < SIZE - 1; i++)
 	{
 		for (int j = 0; j < SIZE - i - 1; j++) {
@@ -16,12 +36,6 @@ int main()
 			}
 		}
 	}
-
-	// print sorted array
-	for (int i = 0; i < SIZE; i++)
-	{
-		std::cout << arr[i] << " ";
-	}
-
-	return 0;
+}
+return 0;
 }
