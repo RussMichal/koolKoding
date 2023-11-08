3e04d1c7fe8400ff5db74efcf2f69d365accc265
bgardner1087 <143228703+bgardner1087@users.noreply.github.com>
2023-11-07 22:06:17 -0600

    Add files via upload

---
+++ Multi Function Homework/Multi Function Homework/Array Functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	const int SIZE = 5;
+	int arr[SIZE] = { 5, 4, 3, 2, 1 };
+
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < SIZE - i - 1; j++) {
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	// print sorted array
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
