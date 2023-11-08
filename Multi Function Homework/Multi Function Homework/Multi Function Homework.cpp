da0e5b1560515a16b778e56cb7cf2b4737fa991c
bgardner1087 <143228703+bgardner1087@users.noreply.github.com>
2023-11-07 21:59:09 -0600

    Add files via upload

---
+++ Multi Function Homework/Multi Function Homework/Multi Function Homework.cpp
@@ -0,0 +1,28 @@
+// Multi Function Homework.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <algorithm> //for ranges
+#include "functions.h"
+
+int main() {
+    const int size = 5;
+    int myArray[size];
+    int x;
+    x = 3;
+
+    // Initialize the array
+    initializeArray(myArray, size);
+
+    // Print the unsorted array
+    std::cout << " ";
+    printArray(myArray, size);
+
+    // Sort the array in ascending order
+    std::sort(myArray, myArray + size);
+    sortingArray(myArray, size);
+    // Print the sorted array
+    std::cout << " ";
+    printArray(myArray, size);
+    return 0;
+}
